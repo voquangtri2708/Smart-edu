604b2fe4da459e1e3e9d8eb19408c655063e5acd
Quang Tri Vo <voquangtri2708@gmail.com>
2025-04-28 19:49:48 +0700

    update: update model classification setiment comment

--- backend/app/ml/preprocess.py
+++ backend/app/ml/preprocess.py
@@ -3,7 +3,7 @@ import pickle
 from pyvi import ViTokenizer
 import os
 
-file_path = os.path.join(os.path.dirname(__file__), "word2id_02.pkl")
+file_path = os.path.join(os.path.dirname(__file__), "word2id.pkl")
 with open(file_path, "rb") as f:
     word2id = pickle.load(f)
 
@@ -18,7 +18,6 @@ def _preprocess_input(text):
 def text2vec(text, max_length=10):
     input_ids = [word2id.get(word, 1) for word in _preprocess_input(text)]  # Chuyển thành ID (1 nếu không có trong từ điển)
 
-    # 2️⃣ Padding/truncate để đảm bảo đúng kích thước đầu vào
     if len(input_ids) < max_length:
         input_ids += [0] * (max_length - len(input_ids))  # Thêm padding
     else:
